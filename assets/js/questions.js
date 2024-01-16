add0f8147de83ad47f11f81045c714a2769eefba
ktotah <ket2137@columbia.edu>
2024-01-16 00:52:40 -0800

    moved my array of questions for the quiz from my script.js file to my questions.js file. also added comments in my js files and html files to explain why i separated my code into multiplefiles of the same type (the separation simplifies the management of different application states (starting the quiz, taking the quiz, viewing scores)).

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+// questions.js
+
+// Separated question data into this file for easier management and modularity.
+// This allows for simpler updates to quiz content and potential reuse in other quizzes.
+
+
+// Array of question objects for the quiz
+const questions = [
+    // QUESTION 1
+    {
+        question: "Who was the Greek goddess of wisdom, warfare, and crafts?",
+        choices: ["A) Aphrodite", "B) Athena", "C) Artemis", "D) Hera"],
+        correct: "B) Athena"
+    },
+
+    // QUESTION 2
+    {
+        question: "Question 2",
+        choices: ["Answer 2a", "Answer 2b", "Answer 2c", "Answer 2d"],
+        correct: "Answer 2c"
+    },
+
+    // QUESTION 3
+    {
+        question: "Question 2",
+        choices: ["Answer 2a", "Answer 2b", "Answer 2c", "Answer 2d"],
+        correct: "Answer 2c"
+    },
+
+     // QUESTION 4
+     {
+        question: "Who is known for completing the twelve labors in Greek mythology?",
+        choices: ["A) Achilles", "B) Perseus", "C) Theseus", "D) Heracles"],
+        correct: "D) Heracles"
+    },
+
+     // QUESTION 5
+     {
+        question: "Question 2",
+        choices: ["Answer 2a", "Answer 2b", "Answer 2c", "Answer 2d"],
+        correct: "Answer 2c"
+    },
+
+     // QUESTION 6
+     {
+        question: "Question 2",
+        choices: ["Answer 2a", "Answer 2b", "Answer 2c", "Answer 2d"],
+        correct: "Answer 2c"
+    },
+
+     // QUESTION 7
+     {
+        question: "Question 2",
+        choices: ["Answer 2a", "Answer 2b", "Answer 2c", "Answer 2d"],
+        correct: "Answer 2c"
+    }
+];
